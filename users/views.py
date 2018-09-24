41692b437a19f6621d5e2eb1621c8a343122c8a9
hilal aissani <hilalaissani@gmail.com>
2018-09-24 13:56:38 -0400

    hh

--- users/views.py
+++ users/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+
 def register(request):
-    print('request.POST==>',request.POST)
+    print('request.POST==>', request.POST)
     if request.method == "POST":
-      form = UserRegisterForm(request.POST)
-      print('form==>', form)
-      if form.is_valid():
-         form.save()
-         username = form.cleaned_data.get('username')
-         print(f'username ===> {username}')
-         messages.success(request, f'=== Account created for {username} ====')
-         return redirect('login')
+        form = UserRegisterForm(request.POST)
+        print('form==>', form)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            print(f'username ===> {username}')
+            messages.success(request, f'=== Account created for {username} ====')
+            return redirect('login')
     else:
-      form = UserRegisterForm()
-   
+        form = UserRegisterForm()
+
     obj = {
-    "form":form
+        "form": form
     }
-    return render(request,'users/register.html',obj)
+    return render(request, 'users/register.html', obj)
 
 
+@login_required
 def profile(request):
-       return render(request, 'users/profile.html' )
+    return render(request, 'users/profile.html')
