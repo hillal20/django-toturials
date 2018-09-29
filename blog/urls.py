865449908531f929fea68b1dc3627da0ef3dc8c1
hilal aissani <hilalaissani@gmail.com>
2018-09-29 10:52:44 -0400

    hh

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views 
+from . import views
+from .views import PostListView
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+
+    # path('', views.home, name='blog-home'), # original version before we use PostListView-built-in way
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
+
+# important
+# <app>/<model>_<viewType>.html === blog/PostViewType.html
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
-
-
+from django.views.generic import ListView
 
 
 def home(request):
-  # return HttpResponse("<h1> blog home </h1>")
+    # return HttpResponse("<h1> blog home </h1>")
 
-  obj = {
-    "posts":Post.objects.all()
-  }
-  return render(request,'blog/home.html', obj) ## django knows templates folder ny default
+    obj = {
+        "posts": Post.objects.all()
+    }
+    # django knows templates folder by default
+    return render(request, 'blog/home.html', obj)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # blog/PostViewType.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
-  # return HttpResponse("<h1> blog about </h1>")
-  return render(request,'blog/about.html', {'title':"About"})
+    # return HttpResponse("<h1> blog about </h1>")
+    return render(request, 'blog/about.html', {'title': "About"})
